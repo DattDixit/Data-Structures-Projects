d24c176a7b39ca2b16e8d74a9b71bd81b267a02b
Datt Dixit <94307881+DattDixit@users.noreply.github.com>
2022-09-02 00:14:57 +0530

    Update main.c

--- Week1-Structures/main.c
+++ Week1-Structures/main.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
-#include "displaymenu.h"
-#include "printBill.h"
+#include "displaymenu.h"//Header file to display menu.
+#include "printBill.h"//Header file to print the bill after order is completed.
 #include "fileIO.h"
 
 int qty0=0, qty1=0, qty2=0, qty3=0, qty4=0, qty5=0;
 int bill;
 
-int main(){
+int main()
+    //for selection of food item from the menu
     display();
     int tst = 1;
     while(tst==1){
@@ -40,7 +41,7 @@ int main(){
         }
         else{
             printf("Do you want to order more? (Yes[1]/No[0]) - ");
-            int resp;
+            int resp;//if the user do not want anything more it will proceed to print the bill
             scanf("%d", &resp);
             if(resp == 0){
                 printInvoice(qty0,qty1,qty2,qty3,qty4,qty5);
